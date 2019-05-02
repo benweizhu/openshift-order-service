9f98e441b8bbdd7c5df69826c6335c74e8d14c79
Benwei Zhu <benweizhu@gmail.com>
2019-05-02 19:42:36 +0800

    init

---
+++ src/main/java/me/zpeh/openshift/order/OpenshiftOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package me.zpeh.openshift.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftOrderServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OpenshiftOrderServiceApplication.class, args);
+  }
+
+}
